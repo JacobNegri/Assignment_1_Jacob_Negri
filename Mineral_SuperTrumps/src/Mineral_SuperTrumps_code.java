4cbfe954e0f73b07c5fabf00818573f6851e15a2
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-09-26 09:12:34 +1000

    showMenu

--- Mineral_SuperTrumps/src/Mineral_SuperTrumps_code.java
+++ Mineral_SuperTrumps/src/Mineral_SuperTrumps_code.java
@@ -7,8 +7,14 @@ public class Mineral_SuperTrumps_code {
 
     public static void main(String[] args) {
         welcomeMessage();
+        showMenu();
     }
     private static void welcomeMessage() {
         System.out.println("hello");
+    }
+
+    private static void showMenu() {
+        System.out.println("1. Start game");
+        System.out.println("2. Exit");
     }
 }
