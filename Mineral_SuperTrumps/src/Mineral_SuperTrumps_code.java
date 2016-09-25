8da4f1576a783469792622567d8c9cdbd52a318b
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-09-26 09:22:59 +1000

    getUserMenuChoice - half complete (need to come back to)

--- Mineral_SuperTrumps/src/Mineral_SuperTrumps_code.java
+++ Mineral_SuperTrumps/src/Mineral_SuperTrumps_code.java
@@ -5,10 +5,25 @@
  */
 public class Mineral_SuperTrumps_code {
 
+    private static final int NEW_GAME = 1;
+    private static final int QUIT_GAME = 2;
+
     public static void main(String[] args) {
         welcomeMessage();
         showMenu();
+        int menuChoice = getUserMenuChoice();
+
+        if(menuChoice == NEW_GAME){
+            startGame();
+        }
     }
+
+    private static int getUserMenuChoice() {
+        // get the user choice and return it
+        // currently just return 1 until above comment is coded
+        return 1;
+    }
+
     private static void welcomeMessage() {
         System.out.println("hello");
     }
@@ -16,5 +31,9 @@ public class Mineral_SuperTrumps_code {
     private static void showMenu() {
         System.out.println("1. Start game");
         System.out.println("2. Exit");
+    }
+
+    private static void startGame() {
+        dealCards();
     }
 }
