ff60f5ebed5e458db7cec262c339005f29d2c243
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-09-26 09:06:24 +1000

    Testing software runs on computer

--- Mineral_SuperTrumps/src/Mineral_SuperTrumps_code.java
+++ Mineral_SuperTrumps/src/Mineral_SuperTrumps_code.java
@@ -1,5 +1,11 @@
+//package Mineral_SuperTrumps;
+
 /**
  * Created by Jacob Negri on 25/09/2016.
  */
 public class Mineral_SuperTrumps_code {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
