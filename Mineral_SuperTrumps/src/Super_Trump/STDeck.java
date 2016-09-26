cd41fc12136f5b24b386fe1f7baca091af71cc40
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-09-26 11:20:58 +1000

    STDeck - trying to sort out the cards and return them through array

--- Mineral_SuperTrumps/src/Super_Trump/STDeck.java
+++ Mineral_SuperTrumps/src/Super_Trump/STDeck.java
@@ -11,9 +11,16 @@ public class STDeck {
     private static final int NUM_CARDS_INIT = 60;
     private ArrayList<STCard> cards;
 
+    // todo: list of all cards
     public STDeck() {
-        ArrayList<STCard> cards =  new ArrayList<STCard>(NUM_CARDS_INIT);
+        cards =  new ArrayList<STCard>();
+
+        for(int i = 0; i < NUM_CARDS_INIT; i++) {
+            cards.add(new STCard());
+            // google how to create random array of integers
+        }
     }
+
     public ArrayList<STCard> dealCards(int i) {
         return null;
     }
