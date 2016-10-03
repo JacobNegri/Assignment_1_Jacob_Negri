e169b78bb021f485910a9c949d9c0fbca5bd9f7c
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-10-03 13:26:54 +1000

    error checking - try statement added

--- Mineral_SuperTrumps/src/Super_Trump/STGame.java
+++ Mineral_SuperTrumps/src/Super_Trump/STGame.java
@@ -86,27 +86,33 @@ public class STGame {
 
         Scanner userSelection = new Scanner(System.in);
 
-        System.out.println("What card due you wish to play \n You may pass if you need");
-        selectOption = userSelection.nextInt();
 
-        while(selectOption < 0 || selectOption > handCount){
+
+        try {
             System.out.println("What card due you wish to play \n You may pass if you need");
             selectOption = userSelection.nextInt();
-        }
 
-        if(selectOption == 0){
-            System.out.println("Turn Passed");
-            ArrayList<STCard> drawnCard = deck.dealCards(1);
+            while (selectOption < 0 || selectOption > handCount) {
+                System.out.println("What card due you wish to play \n You may pass if you need");
+                selectOption = userSelection.nextInt();
+            }
 
-            players[yourPlayerId].playersHand().addAll(drawnCard);
+            if (selectOption == 0) {
+                System.out.println("Turn Passed");
+                ArrayList<STCard> drawnCard = deck.dealCards(1);
 
-            System.out.println(players[yourPlayerId].playersHand());
-        }
-        else {
-            System.out.println(players[yourPlayerId].playersHand().get(selectOption - 1) + " Card is played");
-            players[yourPlayerId].playersHand().remove(selectOption - 1);
+                players[yourPlayerId].playersHand().addAll(drawnCard);
 
-            System.out.println(players[yourPlayerId].playersHand());
+                System.out.println(players[yourPlayerId].playersHand());
+            } else {
+                System.out.println(players[yourPlayerId].playersHand().get(selectOption - 1) + " Card is played");
+                players[yourPlayerId].playersHand().remove(selectOption - 1);
+
+                System.out.println(players[yourPlayerId].playersHand());
+            }
+        } catch (InputMismatchException e){
+            System.out.println("Please Enter a number for the cards position");
+            showPlayerTurn();
         }
     }
 }
